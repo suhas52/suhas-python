e5ac33466c0cd773c9401d9eff3ffc9a055b5213
suhas52 <suhasedu991@gmail.com>
2025-08-22 14:56:58 +0530

    day1 done

---
+++ day-1/bandname/main.py
@@ -0,0 +1,5 @@
+print("Welcome to my first python program")
+birthplace = input("Where were you born?\n")
+petname = input("What's the name of your pet\n")
+
+print("Your band name is " + birthplace + " " + petname + "!")
---
+++ day-1/variables/main.py
@@ -0,0 +1,10 @@
+glass1 = "milk"
+glass2 = "juice"
+
+print("glass1 is " + glass1 + " and glass2 is " + glass2)
+
+tmpglass = glass1
+glass1 = glass2
+glass2 = tmpglass
+
+print("glass1 is " + glass1 + " and glass2 is " + glass2)
