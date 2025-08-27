5f97f624e269e7fcda5b45a29b8579889d072112
suhas52 <suhasedu991@gmail.com>
2025-08-27 21:11:03 +0530

    hi-low complete

--- day-12/hi-low/main.py
+++ day-12/hi-low/main.py
@@ -4,14 +4,17 @@ def get_random_number():
     return random.randint(0, 100)
 
 def get_difficulty():
-    difficulty = input("Please enter 1 for easy mode and 2 for hard mode: ") == "1"
-    if difficulty == "1":
-        return True
-    if difficulty == "2":
-        return False
+    while True:
+        difficulty = input("Please enter 1 for easy mode and 2 for hard mode: ")
+        if difficulty == "1":
+            return 10
+        if difficulty == "2":
+            return 5
+        print("Please enter a valid number.")
+    
     
 def get_user_guess():
-    return input("Please enter your guess between 0 and 100: ")
+    return int(input("Please enter your guess between 0 and 100: "))
 
 def check_high_low(generated_number, user_guess):
     if user_guess < generated_number:
@@ -34,22 +37,19 @@ def replay_logic():
             return False
         print("Please enter a valid letter. (y/n)")
 
+def main_flow(diff_range, generated_number):
+    for i in range(diff_range):
+        user_guess = get_user_guess()
+        if check_high_low(generated_number, user_guess):
+            if i == diff_range - 1:
+                print("You ran out of tries!")
+            continue
+        else:
+            break
+
 replay = replay_logic()
 while replay:
     generated_number = get_random_number()
     difficulty = get_difficulty()
-    if difficulty:
-        for i in range(10):
-            user_guess = get_user_guess()
-            if check_high_low(int(generated_number), int(user_guess)):
-                continue
-            else:
-                break
-    else:
-        for i in range(5):
-            user_guess = get_user_guess()
-            if check_high_low(int(generated_number), int(user_guess)):
-                continue
-            else:
-                break
+    main_flow(difficulty, generated_number)
     replay = replay_logic()
