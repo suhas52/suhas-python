c0b94a5ed1672310160806ce4c9e106d8fc3fb4d
suhas52 <suhasedu991@gmail.com>
2025-08-29 21:11:55 +0530

    oop started

---
+++ day-16/main.py
@@ -0,0 +1,4 @@
+import turtle
+from prettytable import PrettyTable
+
+print(turtle.Turtle)
---
+++ oop/main.py
@@ -0,0 +1,28 @@
+class Item:
+    pay_rate = 0.8
+    all = []
+    def __init__(self, name, price, quantity = 0):
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Price {quantity} is not greater than zero!"
+        
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
+
+item1 = Item("Phone", 100, 5)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
