793c020b6cf2057b3a554074f908dc8aed8a8194
suhas52 <suhasedu991@gmail.com>
2025-09-02 11:40:58 +0000

    turtle graphics

---
+++ day-18/main.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen, Vec2D
+from random import choice
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+colors = ["red","orange","yellow","green","blue","indigo","purple"]
+
+
+def draw_shape(sides, colors):
+	for _ in range(sides):
+		timmy.forward(100)
+		angle = 360 / sides
+		timmy.right(angle)
+	timmy.color(choice(colors))
+
+
+for i in range(3, 15):
+	draw_shape(i, colors)
+
+
+
+
+
+screen = Screen()
+screen.delay(0)
+screen.exitonclick()
